369d5b5109bfd1d546259ee6754b0dee36e494f3
NshimizuN <lolipopgirl723@gmail.com>
2023-05-03 18:00:25 +0900

    新規登録　バリデーション

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -11,8 +11,6 @@ class UserRequest extends FormRequest
      *
      * @return bool
      */
-
-    //フォームリクエスト利用の許可判定
     public function authorize()
     {
          return true;
@@ -26,16 +24,12 @@ class UserRequest extends FormRequest
 
      public function getValidatorInstance()
     {
-        // プルダウンで選択された値(= 配列)を取得
-        $date = $this->input('birth_day', array()); //デフォルト値は空の配列
-
-        // 日付を作成(ex. 2020-1-20)
-        $date_validation = implode('-', $date);
-
-        // rules()に渡す値を追加でセット
-        //     これで、この場で作った変数にもバリデーションを設定できるようになる
+        $old_year = $this->input('old_year');
+        $old_month = $this->input('old_month');
+        $old_day = $this->input('old_day');
+        $date_validation = $old_year . '-' . $old_month . '-' . $old_day;
         $this->merge([
-            'birth_day_validation' => $date_validation,
+            'date_validation' => $date_validation,
         ]);
 
         return parent::getValidatorInstance();
@@ -52,7 +46,7 @@ class UserRequest extends FormRequest
             'under_name_kana' => 'required|string|regex:/^[ア-ン゛゜ァ-ォャ-ョー]+$/u|max:30',
             'mail_address' => 'required|email:filter,dns|max:100|unique:users',
             'sex' =>  ["in:1,2,3"],
-            'birth_day_validation' => 'required|date|before:now|after:2000-01-01',
+            'date_validation' => 'required|date|before:now|after:1999-12-31',
             'role' => ["in:1,2,3,4"],
             'password' => 'required|string|min:8|max:30|confirmed',
             'password_confirmation' => 'required|string|max:191',
@@ -84,10 +78,10 @@ class UserRequest extends FormRequest
 
             'sex.required' => '必須項目です。',
 
-            'birth_day_validation.required'  => '必須項目です。',
-            'birth_day_validation.date'  => '正しい値を入力してください。',
-            'birth_day_validation.before:now'  => '2000年1月1日〜今日までの日付を入力してください。',
-            'birth_day_validation.after:2000-01-01'  => '2000年1月1日〜今日までの日付を入力してください。',
+            'date_validation.required'  => '必須項目です。',
+            'date_validation.date'  => '正しい値を入力してください。',
+            'date_validation.before:now'  => '2000年1月1日〜今日までの日付を入力してください。',
+            'date_validation.after:2000-01-01'  => '2000年1月1日〜今日までの日付を入力してください。',
 
             'role.required' => '必須項目です。',
 
--- resources/views/auth/register/register.blade.php
+++ resources/views/auth/register/register.blade.php
@@ -22,56 +22,65 @@
     <div class="w-100 vh-100 d-flex" style="align-items:center; justify-content:center;">
       <div class="w-25 vh-75 border p-3">
         <div class="register_form">
+
           <div class="d-flex mt-3" style="justify-content:space-between">
             <div class="" style="width:140px">
               <label class="d-block m-0" style="font-size:13px">姓</label>
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 over_name" name="over_name">
               </div>
-              @error('over_name')
-              <li>{{$message}}</li>
-              @enderror
+               @if($errors->first('over_name'))
+               <span class="error_message">{{ $errors->first('over_name') }}</span>
+               @endif
             </div>
+
             <div class="" style="width:140px">
               <label class=" d-block m-0" style="font-size:13px">名</label>
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 under_name" name="under_name">
               </div>
-              @error('under_name')
-              <li>{{$message}}</li>
-              @enderror
+              @if($errors->first('under_name'))
+               <span class="error_message">{{ $errors->first('under_name') }}</span>
+               @endif
             </div>
+
           </div>
+
           <div class="d-flex mt-3" style="justify-content:space-between">
             <div class="" style="width:140px">
               <label class="d-block m-0" style="font-size:13px">セイ</label>
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 over_name_kana" name="over_name_kana">
               </div>
-              @error('over_name_kana')
-              <li>{{$message}}</li>
-              @enderror
+               @if($errors->first('over_name_kana'))
+               <span class="error_message">{{ $errors->first('over_name_kana') }}</span>
+               @endif
             </div>
+
             <div class="" style="width:140px">
               <label class="d-block m-0" style="font-size:13px">メイ</label>
               <div class="border-bottom border-primary" style="width:140px;">
                 <input type="text" style="width:140px;" class="border-0 under_name_kana" name="under_name_kana">
               </div>
-              @error('under_name_kana')
-              <li>{{$message}}</li>
-              @enderror
+              @if($errors->first('under_name_kana'))
+               <span class="error_message">{{ $errors->first('under_name_kana') }}</span>
+               @endif
             </div>
+
           </div>
+
           <div class="mt-3">
             <label class="m-0 d-block" style="font-size:13px">メールアドレス</label>
             <div class="border-bottom border-primary">
               <input type="mail" class="w-100 border-0 mail_address" name="mail_address">
             </div>
-            @error('mail_address')
-            <li>{{$message}}</li>
-            @enderror
+            @if($errors->first('mail_address'))
+               <span class="error_message">{{ $errors->first('mail_address') }}</span>
+               @endif
           </div>
+
         </div>
+
         <div class="mt-3">
           <input type="radio" name="sex" class="sex" value="1">
           <label style="font-size:13px">男性</label>
@@ -79,10 +88,11 @@
           <label style="font-size:13px">女性</label>
           <input type="radio" name="sex" class="sex" value="3">
           <label style="font-size:13px">その他</label>
-          @error('sex')
-            <li>{{$message}}</li>
-            @enderror
+          @if($errors->first('sex'))
+          <span class="error_message">{{ $errors->first('sex') }}</span>
+          @endif
         </div>
+
         <div class="mt-3">
           <label class="d-block m-0 aa" style="font-size:13px">生年月日</label>
           <select class="old_year" name="old_year">
@@ -166,10 +176,11 @@
             <option value="31">31</option>
           </select>
           <label style="font-size:13px">日</label>
-           @error('birth_day_validation')
-            <li>{{$message}}</li>
-            @enderror
+          @if($errors->first('date_validation'))
+          <span class="error_message">{{ $errors->first('date_validation') }}</span>
+          @endif
         </div>
+
         <div class="mt-3">
           <label class="d-block m-0" style="font-size:13px">役職</label>
           <input type="radio" name="role" class="admin_role role" value="1">
@@ -189,25 +200,28 @@
             <label>{{ $subject->subject }}</label>
           </div>
           @endforeach
-          @error('role')
-            <li>{{$message}}</li>
-            @enderror
+           @if($errors->first('role'))
+          <span class="error_message">{{ $errors->first('role') }}</span>
+          @endif
         </div>
+
         <div class="mt-3">
           <label class="d-block m-0" style="font-size:13px">パスワード</label>
           <div class="border-bottom border-primary">
             <input type="password" class="border-0 w-100 password" name="password">
           </div>
-            @error('password')
-            <li>{{$message}}</li>
-            @enderror
+          @if($errors->first('password'))
+          <span class="error_message">{{ $errors->first('password') }}</span>
+          @endif
         </div>
+
         <div class="mt-3">
           <label class="d-block m-0" style="font-size:13px">確認用パスワード</label>
           <div class="border-bottom border-primary">
             <input type="password" class="border-0 w-100 password_confirmation" name="password_confirmation">
           </div>
         </div>
+
         <div class="mt-5 text-right">
           <input type="submit" class="btn btn-primary register_btn" disabled value="新規登録" onclick="return confirm('登録してよろしいですか？')">
         </div>
