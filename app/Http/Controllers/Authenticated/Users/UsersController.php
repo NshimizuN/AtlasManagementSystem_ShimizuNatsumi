5b5289e6368dde86a6c758a779feeafbfd60d31e
NshimizuN <lolipopgirl723@gmail.com>
2023-05-11 00:09:13 +0900

    ユーザー情報一覧の表示項目に選択科目を追加する

--- app/Http/Controllers/Authenticated/Users/UsersController.php
+++ app/Http/Controllers/Authenticated/Users/UsersController.php
@@ -14,25 +14,28 @@ use App\Searchs\SearchResultFactories;
 class UsersController extends Controller
 {
 
+    //searchの検索機能
     public function showUsers(Request $request){
         $keyword = $request->keyword;
         $category = $request->category;
         $updown = $request->updown;
         $gender = $request->sex;
         $role = $request->role;
-        $subjects = null;// ここで検索時の科目を受け取る
+        $subjects = // ここで検索時の科目を受け取る
         $userFactory = new SearchResultFactories();
         $users = $userFactory->initializeUsers($keyword, $category, $updown, $gender, $role, $subjects);
         $subjects = Subjects::all();
         return view('authenticated.users.search', compact('users', 'subjects'));
     }
 
+    //profileにユーザー情報を表示する
     public function userProfile($id){
         $user = User::with('subjects')->findOrFail($id);
         $subject_lists = Subjects::all();
         return view('authenticated.users.profile', compact('user', 'subject_lists'));
     }
 
+    //profileで選択科目を追加、変更する
     public function userEdit(Request $request){
         $user = User::findOrFail($request->user_id);
         $user->subjects()->sync($request->subjects);
